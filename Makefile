c0219edd5929c2efdf304f4b8d334f69b7b97797
Smus4 <raoo@itu.dk>
2022-11-23 16:06:03 +0100

    things looking pretty damn okay kures skal køre fra frederiksberg klokken 17 roger doger Co-authored-by: bemillant <bemillant@users.noreply.github.com> Co-authored-by: thekure <thekure@users.noreply.github.com>

--- Makefile
+++ Makefile
@@ -1,3 +1,11 @@
 gen:
-
 	protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative grpc/interface.proto
+
+server0:
+	go run server/server.go 0
+	
+server1:
+	go run server/server.go 1
+
+server2:
+	go run server/server.go 2
--- client/client.go
+++ client/client.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"bufio"
-	"flag"
+	"context"
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 
 	auction "github.com/thekure/DisysAucSys/grpc"
 	"google.golang.org/grpc"
@@ -15,7 +16,7 @@ import (
 type Client struct {
 	name              string
 	currentHighestBid int32
-	connection        *grpc.ClientConn
+	servers           map[int32]auction.AuctionClient
 }
 
 func main() {
@@ -24,16 +25,22 @@ func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Split(bufio.ScanBytes)
 
+	// Set client name:
 	tempName := ""
 	for scanner.Scan() {
-		tempName += scanner.Text()
+		if scanner.Text() == "\n" {
+			break
+		} else {
+			tempName += scanner.Text()
+		}
 	}
 
-	flag.Parse()
+	// flag.Parse()
 
 	client := &Client{
 		name:              tempName,
 		currentHighestBid: 0,
+		servers:           make(map[int32]auction.AuctionClient),
 	}
 
 	go handleClient(client)
@@ -41,30 +48,99 @@ func main() {
 	for {
 
 	}
+
+}
+
+func (client *Client) makeBid(amount int32) {
+	bid := &auction.RequestBid{
+		Name:    client.name,
+		Message: client.name + " has made the following bid: " + string(amount),
+		Amount:  amount,
+	}
+
+	resultList := make([]string, 0, 3)
+
+	for port, server := range client.servers {
+		ack, err := server.Bid(context.Background(), bid)
+		if err != nil {
+			delete(client.servers, port)
+			// fmt.Printf("something went wrong in bid method: %v", err)
+		}
+		resultList = append(resultList, ack.GetMessage())
+	}
+	fmt.Printf(resultList[0] + "\n")
+}
+
+func (client *Client) requestHighestBid() {
+	reqBid := &auction.HighestBidRequest{
+		Message: client.name + " requested the current bid value.",
+	}
+	// outcome, err := client.connection.Result(context.Background(), reqBid)
+	// if err != nil {
+	// 	log.Printf(err.Error())
+	// }
+
+	resultList := make([]string, 0, 3)
+
+	for port, server := range client.servers {
+		outcome, err := server.Result(context.Background(), reqBid)
+		if err != nil {
+
+			delete(client.servers, port)
+			// fmt.Printf("something went wrong in requestHB method: %v", err)
+		}
+		resultList = append(resultList, outcome.GetStatus())
+	}
+	fmt.Printf(resultList[0] + "\n")
+}
+
+func (client *Client) sendMessage() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for scanner.Scan() {
+		input := scanner.Text()
+
+		//Checks if the client input is of type string og integer (Tries to convert/parse, if an error occurs = string is not parseable)
+		amount, err := strconv.ParseInt(input, 10, 32)
+		if err != nil {
+			//Tell client that the current bid is at ... and to make a bid, type an integer
+			client.requestHighestBid()
+		} else {
+			client.makeBid(int32(amount))
+		}
+	}
 }
 
 func handleClient(client *Client) {
+	client.getServerConnection()
 
-}
+	go client.sendMessage()
 
-func (client *Client) getServerConnection() (auction.AuctionClient, error) {
+	for {
 
-	//dial options
-	//the server is not using TLS, so we use insecure credentials
-	var opts []grpc.DialOption
-	opts = append(opts, grpc.WithBlock(), grpc.WithTransportCredentials(insecure.NewCredentials()))
-
-	conn, err := grpc.Dial("192.168.0.153:5400", opts...) //smusHjemme
-
-	if err != nil {
-		log.Fatalln("Could not dial")
 	}
-	client.connection = conn
-	log.Printf("--- Client is connected to the server ---")
 
-	return auction.NewAuctionClient(conn), err
 }
 
-// func (client *Client) Bid(ctx context.Context) (*auction.Ack, error) {
+func (client *Client) getServerConnection() {
 
-// }
+	for i := 0; i < 3; i++ {
+
+		port := int32(5001) + int32(i)
+		var conn *grpc.ClientConn
+
+		fmt.Printf("Trying to dial: %v\n", port)
+		insecure := insecure.NewCredentials()
+		conn, err := grpc.Dial(fmt.Sprintf(":%v", port), grpc.WithTransportCredentials(insecure), grpc.WithBlock())
+		if err != nil {
+			log.Fatalf("Could not connect: %s", err)
+		}
+
+		log.Printf("--- Succesfully dialed to %v\n", port)
+
+		// defer conn.Close()
+		c := auction.NewAuctionClient(conn)
+		client.servers[port] = c
+	}
+
+}
